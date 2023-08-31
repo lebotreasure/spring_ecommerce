64f45e57382c99667b80208cc49babd577313a8e
lebotreasure <lebohangsylviak@gmail.com>
2023-08-31 12:47:02 +0200

    Initial commit

---
+++ src/main/java/com/ecommerce/kidsclothing/KidsclothingApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kidsclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsclothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsclothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/kidsclothing/KidsclothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kidsclothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsclothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
